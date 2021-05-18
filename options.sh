e218bc834f7a219000ab4a5c885db242f3c5d295
haberda <25726974+haberda@users.noreply.github.com>
2021-05-17 19:01:52 -0700

    Update options.sh

--- options.sh
+++ options.sh
@@ -5,8 +5,16 @@ CONFIG_PATH=/data/options.json
 
 export USE_NATIVE=$(jq --raw-output '.native_mode // empty' $CONFIG_PATH)
 
-export AUTO_RECEIVE_SCHEDULE=$(jq --raw-output '.AUTO_RECEIVE_SCHEDULE // empty' $CONFIG_PATH)
+export AUTO_RECEIVE_SCHEDULE_bool=$(jq --raw-output '.AUTO_RECEIVE // empty' $CONFIG_PATH)
 
+if [ $AUTO_RECEIVE_SCHEDULE_bool == '1' ]
+then
+  export AUTO_RECEIVE_SCHEDULE="0 22 * * *"
+fi
+
+echo "Native mode"
 echo "${USE_NATIVE}"
+echo "AUTO RECEIVE SCHEDULE"
+echo "${AUTO_RECEIVE_SCHEDULE}"
 
 sh /entrypoint.sh
