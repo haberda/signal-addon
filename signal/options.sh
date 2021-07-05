468b25a09a805daada718648046d43767a5e4c94
Daniel Haber <haberda@gmail.com>
2021-07-05 10:55:32 -0700

    Added option to set SIGNAL_CLI_CMD_TIMEOUT

--- signal/options.sh
+++ signal/options.sh
@@ -7,14 +7,18 @@ export USE_NATIVE=$(jq --raw-output '.native_mode // empty' $CONFIG_PATH)
 
 export AUTO_RECEIVE_SCHEDULE_bool=$(jq --raw-output '.AUTO_RECEIVE // empty' $CONFIG_PATH)
 
+export SIGNAL_CLI_CMD_TIMEOUT=$(jq --raw-output '.SIGNAL_CLI_CMD_TIMEOUT // empty' $CONFIG_PATH)
+
 if [ $AUTO_RECEIVE_SCHEDULE_bool == '1' ]
 then
   export AUTO_RECEIVE_SCHEDULE="0 22 * * *"
 fi
 
-echo "Native mode"
+echo "Native mode:"
 echo "${USE_NATIVE}"
-echo "AUTO RECEIVE SCHEDULE"
+echo "AUTO RECEIVE SCHEDULE:"
 echo "${AUTO_RECEIVE_SCHEDULE}"
+echo "Signal-cli command timeout:"
+echo $SIGNAL_CLI_CMD_TIMEOUT
 
 sh /entrypoint.sh
