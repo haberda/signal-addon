3578e7cd8cfc94a10baecfedd7fb88d854c1d24f
haberda <haberda@gmail.com>
2021-12-22 12:39:37 -0800

    Update startup scripts

--- signal/options.sh
+++ signal/options.sh
@@ -3,31 +3,41 @@ set -e
 
 CONFIG_PATH=/data/options.json
 
-#export USE_NATIVE=$(jq --raw-output '.native_mode // empty' $CONFIG_PATH)
-
 export MODE=$(jq --raw-output '.mode // empty' $CONFIG_PATH)
 
 export AUTO_RECEIVE_SCHEDULE_bool=$(jq --raw-output '.AUTO_RECEIVE // empty' $CONFIG_PATH)
 
 export SIGNAL_CLI_CMD_TIMEOUT_tmp=$(jq --raw-output '.SIGNAL_CLI_CMD_TIMEOUT // empty' $CONFIG_PATH)
 
-if [ $AUTO_RECEIVE_SCHEDULE_bool == '1' ]
+export reset_data=$(jq --raw-output '.reset_data // empty' $CONFIG_PATH)
+
+if [ $reset_data ]
 then
-  export AUTO_RECEIVE_SCHEDULE="0 22 * * *"
+	rm -r /data/*
 fi
 
-if [ $SIGNAL_CLI_CMD_TIMEOUT_tmp -ne 0 ]
+if [ $MODE -ne "json-rpc" ]
 then
-  export SIGNAL_CLI_CMD_TIMEOUT=$SIGNAL_CLI_CMD_TIMEOUT_tmp
+
+	if [ $AUTO_RECEIVE_SCHEDULE_bool == '1' ]
+	then
+	  export AUTO_RECEIVE_SCHEDULE="0 22 * * *"
+	fi
+
+	if [ $SIGNAL_CLI_CMD_TIMEOUT_tmp -ne 0 ]
+	then
+	  export SIGNAL_CLI_CMD_TIMEOUT=$SIGNAL_CLI_CMD_TIMEOUT_tmp
+	fi
 fi
 
-echo "Native mode:"
-echo "${USE_NATIVE}"
 echo "Mode:"
 echo "${MODE}"
-echo "AUTO RECEIVE SCHEDULE:"
-echo "${AUTO_RECEIVE_SCHEDULE}"
-echo "Signal-cli command timeout:"
-echo $SIGNAL_CLI_CMD_TIMEOUT
+if [ $MODE -ne "json-rpc" ]
+then
+	echo "AUTO RECEIVE SCHEDULE:"
+	echo "${AUTO_RECEIVE_SCHEDULE}"
+	echo "Signal-cli command timeout:"
+	echo "$SIGNAL_CLI_CMD_TIMEOUT"
+fi
 
 sh /entrypoint.sh
