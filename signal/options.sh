43502f6f8b3061cd27b5dc2acb2aef6c67124c24
Daniel Haber <haberda@gmail.com>
2024-02-05 16:35:54 -0800

    Migrate data storage to new addon_config location
    Also remove reset data option because the user can now manage their own data.

--- signal/options.sh
+++ signal/options.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+if [ -d /data/data ]; then
+  mv /data/* /config
+  mv /config/options.json /data
+fi
+
 CONFIG_PATH=/data/options.json
 
 export MODE_tmp=$(jq --raw-output '.mode // empty' $CONFIG_PATH)
@@ -11,13 +16,13 @@ export SIGNAL_CLI_CMD_TIMEOUT_tmp=$(jq --raw-output '.SIGNAL_CLI_CMD_TIMEOUT // 
 
 export reset_data=$(jq --raw-output '.reset_data // empty' $CONFIG_PATH)
 
-if [ $reset_data ]
-then
-	rm -r /data/*
-	echo "Data deleted. Please set reset_data to off and restart the addon."
-	exit
-fi
-
+#if [ $reset_data ]
+#then
+#	rm -r /data/*
+#	echo "Data deleted. Please set reset_data to off and restart the addon."
+#	exit
+#fi
+#
 export MODE=$(jq --raw-output '.mode // empty' $CONFIG_PATH)
 
 if [ "${MODE_tmp}" != "json-rpc" ]; then
