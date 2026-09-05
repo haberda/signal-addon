ec74f6916563def1599808c5956186f8b98644a4
Daniel Haber <haberda@gmail.com>
2026-09-05 10:45:38 -0700

    Enable stricter linting

--- signal/options.sh
+++ signal/options.sh
@@ -3,17 +3,17 @@ set -e
 
 CONFIG_PATH=/data/options.json
 
-export MODE_tmp=$(jq --raw-output '.mode // empty' "$CONFIG_PATH")
+MODE_tmp=$(jq --raw-output '.mode // empty' "$CONFIG_PATH")
 
-export AUTO_RECEIVE_SCHEDULE_bool=$(jq --raw-output '.AUTO_RECEIVE // empty' "$CONFIG_PATH")
+AUTO_RECEIVE_SCHEDULE_bool=$(jq --raw-output '.AUTO_RECEIVE // empty' "$CONFIG_PATH")
 
-export SIGNAL_CLI_CMD_TIMEOUT_tmp=$(jq --raw-output '.SIGNAL_CLI_CMD_TIMEOUT // empty' "$CONFIG_PATH")
+SIGNAL_CLI_CMD_TIMEOUT_tmp=$(jq --raw-output '.SIGNAL_CLI_CMD_TIMEOUT // empty' "$CONFIG_PATH")
 
-export reset_data=$(jq --raw-output '.reset_data // empty' "$CONFIG_PATH")
+MODE=$(jq --raw-output '.mode // empty' "$CONFIG_PATH")
+export MODE
 
-export MODE=$(jq --raw-output '.mode // empty' "$CONFIG_PATH")
-
-export DEFAULT_SIGNAL_TEXT_MODE=$(jq --raw-output '.DEFAULT_SIGNAL_TEXT_MODE // "normal"' $CONFIG_PATH)
+DEFAULT_SIGNAL_TEXT_MODE=$(jq --raw-output '.DEFAULT_SIGNAL_TEXT_MODE // "normal"' "$CONFIG_PATH")
+export DEFAULT_SIGNAL_TEXT_MODE
 
 if [ "${MODE_tmp}" != "json-rpc" ] && [ "${MODE_tmp}" != "json-rpc-native" ]; then
 
@@ -29,4 +29,4 @@ if [ "${MODE_tmp}" != "json-rpc" ] && [ "${MODE_tmp}" != "json-rpc-native" ]; th
 
 fi
 
-sh /entrypoint.sh
+exec /entrypoint.sh
